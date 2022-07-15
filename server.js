d1b42498750e76b9f69d396f3f566bb34b2ef03d
Ruchi Agrawal <ruagrawa@microsoft.com>
2022-07-15 14:12:14 +0530

    Adding express router

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 
-const messagesController = require('./controllers/messages.controller');
-const friendsController = require('./controllers/friends.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 const PORT = 3000;
 
-
 //custom middleware for logging
 app.use((req, res, next) => {
    const start = Date.now();
-   console.log(`Request Attributes : ${req.method} ${req.url}`);
+   console.log(`Request Attributes : ${req.method} ${req.baseUrl}${req.url}`);
    next();
    //actions go here....
    const delta = Date.now() - start;
@@ -23,13 +22,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 })
 
-app.get("/friends", friendsController.getFriends);
-
-app.post('/friends', friendsController.addFriend);
-
-app.get("/friends/:friendId", friendsController.getFriend);
-
-app.get("/messages", messagesController.getMessages);
+app.use("/friends", friendsRouter);
+app.get("/messages", messagesRouter);
 
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`);
